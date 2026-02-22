f4f6affbd46a2c195e0aedbe1ddb755f0782c58e
Andre Souza <asouza1807@gmail.com>
2026-02-22 19:48:05 -0300

    ajuste tela cadastro e formulario cadastro

--- allocations/views.py
+++ allocations/views.py
@@ -7,14 +7,13 @@ from django.views.generic import TemplateView, View
 from core.mixins import RoleRequiredMixin
 from core.exceptions.domain_exceptions import BusinessRuleException
 from core.services.allocation_service import AllocationService
-from employees.forms import EmployeeForm
 from employees.models import Employee
-from telecom.forms import SIMcardForm, PhoneLineForm, CombinedSimLineForm
+from employees.forms import EmployeeForm
 from telecom.models import PhoneLine, SIMcard
 from users.models import SystemUser
 
 from .models import LineAllocation
-from .forms import AllocationForm
+from .forms import TelephonyAssignmentForm
 
 
 class RegistrationHubView(RoleRequiredMixin, TemplateView):
@@ -25,13 +24,8 @@ class RegistrationHubView(RoleRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context['employee_form'] = kwargs.get(
             'employee_form') or EmployeeForm()
-        context['simcard_form'] = kwargs.get('simcard_form') or SIMcardForm()
-        context['phoneline_form'] = kwargs.get(
-            'phoneline_form') or PhoneLineForm()
-        context['allocation_form'] = kwargs.get(
-            'allocation_form') or AllocationForm()
-        context['combined_sim_line_form'] = kwargs.get(
-            'combined_sim_line_form') or CombinedSimLineForm()
+        context['telephony_form'] = kwargs.get(
+            'telephony_form') or TelephonyAssignmentForm()
         context['allocations'] = self._allocations_qs()
         context['available_lines'] = self._available_lines_qs()
         context['available_simcards'] = self._available_simcards_qs()
@@ -40,21 +34,14 @@ class RegistrationHubView(RoleRequiredMixin, TemplateView):
 
     def post(self, request, *args, **kwargs):
         action = request.POST.get('action')
-        handlers = {
-            'employee': self._handle_employee,
-            'simcard': self._handle_simcard,
-            'phoneline': self._handle_phoneline,
-            'allocation': self._handle_allocation,
-            'simcard_line': self._handle_simcard_line,
-        }
 
-        handler = handlers.get(action)
-        if not handler:
-            messages.error(request, 'Ação inválida.')
-            return redirect('allocations:allocation_list')
+        if action == 'employee':
+            return self._handle_employee(request)
+        if action == 'telephony':
+            return self._handle_telephony(request)
 
-        response = handler(request)
-        return response or redirect('allocations:allocation_list')
+        messages.error(request, 'Ação inválida.')
+        return redirect('allocations:allocation_list')
 
     def _handle_employee(self, request):
         self._ensure_roles(request, [SystemUser.Role.ADMIN])
@@ -62,82 +49,57 @@ class RegistrationHubView(RoleRequiredMixin, TemplateView):
 
         if form.is_valid():
             form.save()
-            messages.success(request, 'Usuário cadastrado com sucesso.')
-            return None
+            messages.success(request, 'Colaborador cadastrado com sucesso.')
+            return redirect('allocations:allocation_list')
 
-        messages.error(request, 'Corrija os erros no cadastro de usuário.')
+        messages.error(request, 'Corrija os erros do colaborador.')
         return self._render_with_forms(employee_form=form)
 
-    def _handle_simcard(self, request):
+    def _handle_telephony(self, request):
         self._ensure_roles(request, [SystemUser.Role.ADMIN])
-        form = SIMcardForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'SIM card cadastrado com sucesso.')
-            return None
-
-        messages.error(request, 'Corrija os erros no cadastro de SIM card.')
-        return self._render_with_forms(simcard_form=form)
-
-    def _handle_phoneline(self, request):
-        self._ensure_roles(request, [SystemUser.Role.ADMIN])
-        form = PhoneLineForm(request.POST)
-
-        if form.is_valid():
-            phone_line = form.save()
-            messages.success(
-                request, f'Linha {phone_line.phone_number} criada com sucesso.')
-            return None
-
-        messages.error(request, 'Corrija os erros no cadastro de linha.')
-        return self._render_with_forms(phoneline_form=form)
-
-    def _handle_simcard_line(self, request):
-        self._ensure_roles(request, [SystemUser.Role.ADMIN])
-        form = CombinedSimLineForm(request.POST)
+        form = TelephonyAssignmentForm(request.POST)
 
         if not form.is_valid():
-            messages.error(
-                request, 'Corrija os erros para salvar linha e SIM card.')
-            return self._render_with_forms(combined_sim_line_form=form)
+            messages.error(request, 'Corrija os erros de telefonia.')
+            return self._render_with_forms(telephony_form=form)
 
-        with transaction.atomic():
-            sim = SIMcard.objects.create(
-                iccid=form.cleaned_data['iccid'],
-                carrier=form.cleaned_data['carrier'],
-                status=SIMcard.Status.AVAILABLE,
-            )
-            phone_line = PhoneLine.objects.create(
-                phone_number=form.cleaned_data['phone_number'],
-                sim_card=sim,
-                status=PhoneLine.Status.AVAILABLE,
-            )
-
-        messages.success(
-            request, f'SIM card e linha {phone_line.phone_number} cadastrados com sucesso.')
-        return None
-
-    def _handle_allocation(self, request):
-        form = AllocationForm(request.POST)
-
-        if not form.is_valid():
-            messages.error(
-                request, 'Preencha os campos obrigatórios para alocar uma linha.')
-            return self._render_with_forms(allocation_form=form)
-
-        employee = form.cleaned_data['employee']
-        phone_line = form.cleaned_data['phone_line']
+        line_action = form.cleaned_data['line_action']
 
         try:
-            AllocationService.allocate_line(
-                employee, phone_line, allocated_by=request.user)
+            with transaction.atomic():
+                if line_action == 'existing':
+                    phone_line = form.cleaned_data['phone_line']
+                    sim = phone_line.sim_card
+                    employee = form.cleaned_data['employee']
+                else:
+                    sim = SIMcard.objects.create(
+                        iccid=form.cleaned_data['iccid'],
+                        carrier=form.cleaned_data['carrier'],
+                        status=SIMcard.Status.AVAILABLE,
+                    )
+
+                    phone_line = PhoneLine.objects.create(
+                        phone_number=form.cleaned_data['phone_number'],
+                        sim_card=sim,
+                        status=PhoneLine.Status.AVAILABLE,
+                    )
+
+                    employee = form.cleaned_data.get('employee')
+
+                if employee:
+                    AllocationService.allocate_line(
+                        employee, phone_line, allocated_by=request.user)
         except BusinessRuleException as exc:
             messages.error(request, str(exc))
-            return self._render_with_forms(allocation_form=form)
+            return self._render_with_forms(telephony_form=form)
 
-        messages.success(request, 'Linha alocada com sucesso.')
-        return None
+        if line_action == 'existing' or form.cleaned_data.get('employee'):
+            messages.success(
+                request, 'Dados de telefonia salvos e linha alocada com sucesso.')
+        else:
+            messages.success(
+                request, 'Dados de telefonia salvos; linha criada sem alocação.')
+        return redirect('allocations:allocation_list')
 
     def _render_with_forms(self, **forms):
         context = self.get_context_data(**forms)
