98ae458c05151772a68d69dcff32db77a01e69b8
Andre Santos De Souza <andre.souza@somosglobal.com.br>
2026-03-06 09:18:11 -0300

    fix: exportar B2B_PORTFOLIO_NAMES e B2C_PORTFOLIO_NAMES no __init__.py
    - Adicionar B2B_PORTFOLIO_NAMES e B2C_PORTFOLIO_NAMES ao import
    - Adicionar normalize_portfolio_name ao import
    - Corrigir ImportError no dashboard/views.py

--- core/constants/__init__.py
+++ core/constants/__init__.py
@@ -1,13 +1,19 @@
 from .portfolio_choices import (
+    B2B_PORTFOLIO_NAMES,
     B2B_PORTFOLIOS,
     B2B_SUPERVISORS,
+    B2C_PORTFOLIO_NAMES,
     B2C_PORTFOLIOS,
     B2C_SUPERVISORS,
+    normalize_portfolio_name,
 )
 
 __all__ = [
+    "B2B_PORTFOLIO_NAMES",
     "B2B_PORTFOLIOS",
     "B2B_SUPERVISORS",
+    "B2C_PORTFOLIO_NAMES",
     "B2C_PORTFOLIOS",
     "B2C_SUPERVISORS",
+    "normalize_portfolio_name",
 ]
